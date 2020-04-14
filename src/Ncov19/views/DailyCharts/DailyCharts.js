375f03c0b9d78f33879d8a2eab76b21f7e8f4a8d
ReymarkTion <reymarktion@gmail.com>
2020-04-14 20:38:25 +0800

    bar graph added

--- src/Ncov19/views/DailyCharts/DailyCharts.js
+++ src/Ncov19/views/DailyCharts/DailyCharts.js
@@ -5,6 +5,7 @@ import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import Footer from '../../components/Footer/Footer';
 import React, { Component } from 'react';
 import { MdPlace } from 'react-icons/md';
+import { IoIosStats } from 'react-icons/io';
 import '../../../_css/sidebar_style.css';
 import Chart from "react-apexcharts";
 
@@ -13,38 +14,6 @@ class DailyCharts extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // fixed
-            place_info: [
-                {
-                    name: "Bukidnon",
-                    selected: false
-                },
-                {
-                    name: "Camiguin",
-                    selected: false
-                },
-                {
-                    name: "Lanao del Norte",
-                    selected: false
-                },
-                {
-                    name: "Misamis Occidental",
-                    selected: false
-                },
-                {
-                    name: "Misamis Oriental",
-                    selected: false
-                },
-                {
-                    name: "Cagayan de Oro City",
-                    selected: false
-                },
-                {
-                    name: "Iligan City",
-                    selected: false
-                }
-            ],
-
 
             // -------------------------------------------------------------
             /**
@@ -240,10 +209,65 @@ class DailyCharts extends Component {
                     date: new Date("March 6, 2020"),
                     ncov_info: { pum: 32, pui: 30, pc: 29, d: 2, r: 35 }
                 },
-            ], 
+            ],
             // -------------------------------------------------------------
 
+            // fixed
+            place_info: [
+                {
+                    name: "Bukidnon",
+                    selected: false
+                },
+                {
+                    name: "Camiguin",
+                    selected: false
+                },
+                {
+                    name: "Lanao del Norte",
+                    selected: false
+                },
+                {
+                    name: "Misamis Occidental",
+                    selected: false
+                },
+                {
+                    name: "Misamis Oriental",
+                    selected: false
+                },
+                {
+                    name: "Cagayan de Oro City",
+                    selected: false
+                },
+                {
+                    name: "Iligan City",
+                    selected: false
+                }
+            ],
 
+            stat: [
+                {
+                    name: "Persons Under Moitoring",
+                    selected: false
+                },
+                {
+                    name: "Persons Under Investigation",
+                    selected: false
+                },
+                {
+                    name: "Positive Cases",
+                    selected: false
+                },
+                {
+                    name: "Deaths",
+                    selected: false
+                },
+                {
+                    name: "Recoveries",
+                    selected: false
+                },
+            ],
+
+            // line chart data
             options: {
                 chart: {
                     height: 350,
@@ -260,7 +284,7 @@ class DailyCharts extends Component {
                         show: false
                     }
                 },
-                colors: ['#620ef1', '#e4f717', '#da0505', "#da5456", "#23a442"],
+                colors: ['#620ef1', '#ffc107', '#da0505', "#da5456", "#23a442"],
                 dataLabels: {
                     enabled: false,
                 },
@@ -326,30 +350,100 @@ class DailyCharts extends Component {
                 }
             ],
 
+            // bar graph data
+            options_bar: {
+                chart: {
+                    type: 'basic-bar',
+                    dropShadow: {
+                        enabled: true,
+                        color: '#000',
+                        top: 18,
+                        left: 7,
+                        blur: 10,
+                        opacity: 0.2
+                    },
+                    toolbar: {
+                        show: false
+                    }
+                },
+                colors: ['#620ef1'],
+                title: {
+                    text: 'Statistics Bar Graph',
+                    align: 'left'
+                },
+                colors: ['#620ef1'],
+                grid: {
+                    borderColor: '#e7e7e7',
+                    row: {
+                        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                        opacity: 0.5
+                    },
+                },
+                markers: {
+                    size: 2
+                },
+                xaxis: {
+                    categories: [],
+                    title: {
+                        text: 'PROVINCE / CHARTERED CITY'
+                    }
+                },
+                yaxis: {
+                    title: {
+                        text: ''
+                    }/*,
+                    min: 0,
+                    max: 40 */
+                }
+            },
+
+            series_bar: [
+                {
+                    name: "",
+                    data: []
+                }
+            ]
+
         };
 
         this.tmonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
     }
 
     componentDidMount = () => {
+        // initialize options bargraph data
+        let Arr = [];
+        (this.state.place_info || []).map(data => {
+            Arr.push(data.name);
+        });
+        this.setState({
+            options_bar: {
+                ...this.state.options_bar, xaxis: {
+                    categories: Arr, title: { text: "PROVINCE / CHARTERED CITY" }
+                }
+            }
+        });
+
+
+
         //console.log("date now -> ",Date.now);
         //console.log("date new -> ", new Date());
-        console.log("dt state -> ", this.state.incov_data[0].date);
+        //console.log("dt state -> ", this.state.incov_data[0].date);
     }
 
-    on_place_highlight = (p) => {
+    on_highlight = (p, state, s) => {
         let Arr = [];
-        (this.state.place_info || []).map((data, index) => {
+        (state || []).map((data, index) => {
             data.selected = false;
             if (p === data.name)
                 data.selected = true;
             Arr.push(data);
         });
-        this.setState({ place_info: Arr });
+        s === "place" ? this.setState({ place_info: Arr }) : this.setState({ stat: Arr });
     }
 
+    //------------------------------------------------
     select_place = (place) => {
-        this.on_place_highlight(place);
+        this.on_highlight(place, this.state.place_info, 'place');
 
         // set daily map data
         // parse data for daily chart display
@@ -358,17 +452,17 @@ class DailyCharts extends Component {
         (this.state.incov_data || []).map((data) => {
             if (data.name === place) {
                 //console.log("cat -> ", data.date);
-                cat.push( this.tmonth[data.date.getMonth()] + " " + data.date.getDate() + ", " + data.date.getFullYear() )
+                cat.push(this.tmonth[data.date.getMonth()] + " " + data.date.getDate() + ", " + data.date.getFullYear())
                 const pum_t = data.ncov_info.pum; const pui_t = data.ncov_info.pui;
-                const pc_t = data.ncov_info.pc;   const d_t = data.ncov_info.d;
+                const pc_t = data.ncov_info.pc; const d_t = data.ncov_info.d;
                 const r_t = data.ncov_info.r;
 
-                pum.push(pum_t);            pui.push(pui_t);
+                pum.push(pum_t); pui.push(pui_t);
                 pc.push(data.ncov_info.pc); d.push(d_t);
                 r.push(r_t);
-                
+
                 pum_t > max ? max = pum_t : max = max; pui_t > max ? max = pui_t : max = max;
-                pc_t > max ? max = pc_t : max = max;   d_t > max ? max = d_t : max = max;
+                pc_t > max ? max = pc_t : max = max; d_t > max ? max = d_t : max = max;
                 r_t > max ? max = r_t : max = max;
             }
         });
@@ -406,12 +500,13 @@ class DailyCharts extends Component {
         ];
 
         //console.log("series -> ", series);
-        this.setState({ series: series, 
-            options: { 
-                ...this.state.options, 
+        this.setState({
+            series: series,
+            options: {
+                ...this.state.options,
                 yaxis: { title: { text: "Number" }, min: 0, max: max + 5 },
-                xaxis: { categories: cat, title: { text: 'Date' } }, 
-                title: { text: place + " - Daily Records", align: 'left' } 
+                xaxis: { categories: cat, title: { text: 'Date' } },
+                title: { text: place + " - Daily Records", align: 'left' }
             },
         });
     }
@@ -464,6 +559,124 @@ class DailyCharts extends Component {
         );
     }
 
+    // -----------------------------------------------
+
+    select_stat = (st) => {
+        this.on_highlight(st, this.state.stat, 'stat');
+
+        const place_num = this.state.place_info.length;
+        const latest_data = this.state
+            .incov_data
+            .slice(this.state.incov_data.length - place_num, this.state.incov_data.length);
+
+        let cat = [];
+        let sb = [];
+        let max = 0;
+        let color = '#620ef1';
+        (latest_data || []).map(data => {
+            cat.push(data.name);
+            switch (st) {
+                case "Persons Under Moitoring":
+                    color = '#620ef1';
+                    data.ncov_info.pum > max ? max = data.ncov_info.pum : max = max;
+                    sb.push(data.ncov_info.pum);
+                    break;
+                case "Persons Under Investigation":
+                    color = '#ffc107';
+                    data.ncov_info.pui > max ? max = data.ncov_info.pui : max = max;
+                    sb.push(data.ncov_info.pui);
+                    break;
+                case "Positive Cases":
+                    color = '#da0505';
+                    data.ncov_info.pc > max ? max = data.ncov_info.pc : max = max;
+                    sb.push(data.ncov_info.pc);
+                    break;
+                case "Deaths":
+                    color = "#da5456";
+                    data.ncov_info.d > max ? max = data.ncov_info.d : max = max;
+                    sb.push(data.ncov_info.d);
+                    break;
+                case "Recoveries":
+                    color = '#23a442';
+                    data.ncov_info.r > max ? max = data.ncov_info.r : max = max;
+                    sb.push(data.ncov_info.r);
+                    break;
+            }
+        });
+
+        //console.log(color);
+        this.setState({
+            options_bar: {
+                ...this.state.options_bar, xaxis: {
+                    categories: cat, title: { text: "PROVINCE / CHARTERED CITY" }
+                },
+                yaxis: { title: { text: "" }, min: 0, max: max + 5 },
+                colors: [color]
+            },
+            series_bar: [
+                { name: st, data: sb }
+            ]
+        });
+
+
+
+
+    }
+
+    render_stats = () => {
+        const data = this.state.incov_data[this.state.incov_data.length - 1];
+        return (
+            <>
+                <p class="h6 text-center text-info mt-4">
+                    <div className="d-flex">
+                        <h4><IoIosStats /></h4>
+                        <strong className="flex-grow-1 mt-2">STATISTICS</strong>
+                    </div>
+                    {this.tmonth[data.date.getMonth()] + " " + data.date.getDate() + ", " + data.date.getFullYear()}
+                </p>
+                <ul class="list-group list-group-flush">
+                    {
+                        (this.state.stat || []).map((data, index) => {
+                            return (
+                                <a type="button" onClick={() => this.select_stat(data.name)}>
+                                    <li class={"list-group-item ".concat(data.selected ? "bg-dark text-light" : "")}>
+                                        <strong>{data.name}</strong>
+                                    </li>
+                                </a>
+                            )
+                        })
+                    }
+                </ul>
+            </>
+        );
+    }
+
+    render_chart_bar = () => {
+        return (
+            <>
+                <br />
+                <div className="row">
+                    <div className="col-lg-1 col-xl-1"></div>
+                    <div className="col-lg-10 col-xl-10 col-sm-12 col-md-12">
+                        <div class="card">
+                            <div className="card-body">
+                                <Chart
+                                    options={this.state.options_bar}
+                                    series={this.state.series_bar}
+                                    type="bar"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-1 col-xl-1"></div>
+                </div>
+            </>
+        )
+    }
+
+    // -----------------------------------------------
+
+
     render_mainComponent() {
         return (
             <>
@@ -478,8 +691,16 @@ class DailyCharts extends Component {
                         <div className="col-sm-9">
                             {this.render_chart()}
                         </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col-sm-3">
+                            {this.render_stats()}
+                        </div>
 
-
+                        <div className="col-sm-9">
+                            {this.render_chart_bar()}
+                        </div>
                     </div>
                 </div>
                 <br /><br /><br /><br /><br />
