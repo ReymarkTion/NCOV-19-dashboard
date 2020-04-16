cac77c18d8fa7ccb18632b105c875aa1c2dcaab7
ReymarkTion <reymarktion@gmail.com>
2020-04-16 18:56:10 +0800

    auto redirect to /ncov19

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Route, Router, Switch } from 'react-router-dom';
+import { Route, Router, Switch, Redirect } from 'react-router-dom';
 
 import { history, withAuth } from '../_helper';
 import Ncov19 from '../Ncov19/layouts/Ncov19';
-//import ICPEP_SE from '../ICPEP_SE/layouts/ICPEP_SE';
+import ICPEP_SE from '../ICPEP_SE/layouts/ICPEP_SE';
 
 export class App extends React.Component {
   render() {
@@ -13,6 +13,7 @@ export class App extends React.Component {
         <Switch>
           <Route path="/ncov19" component={ Ncov19 }/>
           {/*<Route path="/" component={ ICPEP_SE } />*/}
+          <Redirect from="/" to="/ncov19/dashboard" />
           <Route path="/" render={() => <div>404</div>}/>
         </Switch>
       </Router>
